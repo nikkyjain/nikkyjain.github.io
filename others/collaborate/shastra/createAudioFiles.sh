4f5aed65b75817498638bd2e07fbd7d03ca35c3b
Vishal <nikkyjain@gmail.com>
2020-06-14 18:04:50 +0530

    Added gatha

--- others/collaborate/shastra/createAudioFiles.sh
+++ others/collaborate/shastra/createAudioFiles.sh
@@ -2,7 +2,7 @@
 
 outDir=$(cd ../../.. && echo $PWD)
 myAudioJs=$outDir/js/allAudioFiles.js
-
+/bin/rm -f $myAudioJs
 for audio in $(find $outDir/jainDataBase/bhajans/. -name '*.mp3')
 do
   myAudioFile=$(echo $audio | perl -pe 's|.*?jainData|.|')
